0821cf152b40eb13a68eb60d746d3f44c4a2f1fa
Julien Karst <contact@julienkarst.com>
2020-10-23 09:04:30 +0200

    chore: minimal reproduction bug

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,39 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+// import * as core from '@actions/core'
+// import * as github from '@actions/github'
 import lint from '@commitlint/lint'
 import load from '@commitlint/load'
 
 async function run(): Promise<void> {
   try {
-    const configPath = core.getInput('configuration-path', {required: true})
-    const title = getPrTitle()
+    // const configPath = core.getInput('configuration-path', {required: true})
+    const configPath = './commitlint.config.js'
+    const title = 'feat: pr name'
     if (!title) {
-      core.debug('Could not get pull request title from context, exiting')
+      // core.debug('Could not get pull request title from context, exiting')
       return
     }
     await lintPullRequest(title, configPath)
   } catch (error) {
-    core.error(error)
-    core.setFailed(error.message)
+    // core.error(error)
+    // core.setFailed(error.message)
   }
 }
 
-function getPrTitle(): string | undefined {
-  const pullRequest = github.context.payload.pull_request
-  if (!pullRequest) {
-    return undefined
-  }
-
-  return pullRequest.title
-}
+// function getPrTitle(): string | undefined {
+//   const pullRequest = github.context.payload.pull_request
+//   if (!pullRequest) {
+//     return undefined
+//   }
+//
+//   return pullRequest.title
+// }
 
 export async function lintPullRequest(title: string, configPath: string) {
   const opts = await load({}, {file: configPath, cwd: process.cwd()})
 
-  const result = await lint(
-    title,
-    opts.rules,
-    opts.parserPreset ? {parserOpts: opts.parserPreset.parserOpts} : {}
-  )
-  if (result.valid === true) return
+  const result = await lint(title, opts.rules, opts)
+
+  if (result.valid) return
 
   const errorMessage = result.errors
     .map(({message, name}: any) => `${name}:${message}`)
